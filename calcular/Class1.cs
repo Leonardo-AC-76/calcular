4b0b07d24146ed2435515473359266e7fb9e1e8f
1dawba <1dawba@informatica.edu>
2021-03-25 12:09:56 +0000

    Agregar archivos de proyecto.

---
+++ calcular/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcular
+{
+    public class Calculadora
+    {
+        public static int suma(int num1, int num2)
+        {
+            return (num1 + num2);
+        }
+
+        public static int multiplicacion(int num1, int num2)
+        {
+            return (num1 * num2);
+        }
+    }
+}
---
+++ calcular/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proyecto de calculo");
+
+            int operador1 = 10;
+            int operador2 = 5;
+
+            int suma = Calculadora.suma(operador1, operador2);
+            int multiplicacion = Calculadora.multiplicacion(operador1, operador2);
+
+
+        }
+    }
+}
